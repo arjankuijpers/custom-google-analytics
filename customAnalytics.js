7e7c26c36f9796ffe80e862cb0fab4eab0b624b2
Arjan Kuijpers <arjankuijpers@live.nl>
2014-12-04 17:26:16 +0100

    bug fixes semi-colom need to be insert. also function checked for cocoonJS namespace instead of Distlib.GoogleAnalytics.

--- customAnalytics.js
+++ customAnalytics.js
@@ -9,8 +9,8 @@ CustomAnalytics = function(trackingId, clientId, osInfo, language)
     this.AnalyticsURL = "https://google-analytics.com/collect";
     this.measurementVersion = 1;
     this.cid = clientId || 0;
-	this.ul = language || this._getLanguage();
-	this.osInfo = osInfo || "null";
+    this.ul = language || this._getLanguage();
+    this.osInfo = osInfo || "null";
 }
 
 CustomAnalytics.prototype = {
@@ -18,31 +18,31 @@ CustomAnalytics.prototype = {
         //init the Analytics
         console.log("CA: initialize Custom Analytics");
     },
-    
+
     setDimension: function(fieldName, value){
-        console.log("CA: setDimension not implemented"); 
+        console.log("CA: setDimension not implemented");
     },
-    
+
     sendPageView:function(page){
         var params = "dh=CustomAnalytics.app&dp=" + page + "&dt="+page;
         this._postToService("pageview", params);
     },
-    
+
     sendEvent: function(category, action, label, value){
         var params = "ec="+category+"&ea="+action+"&el="+label+"&ev="+value;
         this._postToService("event", params);
     },
-    
+
     _postToService: function(hitType, aditionalParams, callback) {
         var params = "v="+this.measurementVersion +
             "&tid="+ this.trackingID +
             "&cid="+this.cid +
             "&t="+hitType +
-			"&ul="+this.ul +
-			"&cm[1][0]*=" + this.osInfo + // Custom metric, you can specify it with what you want.
+            "&ul="+this.ul +
+            "&cm[1][0]*=" + this.osInfo + // Custom metric, you can specify it with what you want.
             "&"+aditionalParams;
-        
-        
+
+
         var trackingPixel = new Image();
         trackingPixel.onload = function()
         {
@@ -55,21 +55,21 @@ CustomAnalytics.prototype = {
         trackingPixel.src = this.AnalyticsURL + "?" + params;
         console.log(trackingPixel.src);
     },
-	
-	_getLanguage: function() {
-		return navigator.language ? navigator.language : null;
-	},
-	
-	setupOS: function(osString) {
-		this.osInfo = osString;
-	},
-}
+
+    _getLanguage: function() {
+        return navigator.language ? navigator.language : null;
+    },
+
+    setupOS: function(osString) {
+        this.osInfo = osString;
+    },
+};
 
 
 (function() {
     console.log("try to overwrite distlib");
 
-    if((typeof CocoonJS === "undefined")) {
+    if((typeof DistLib.GoogleAnalytics === "undefined")) {
         console.error("Distlib not yet loaded, this needs to be loaded after distlib");
         return false;
     }
