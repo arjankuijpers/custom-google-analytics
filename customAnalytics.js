73a5b605ef325b7a392ed8d446a0eac483a608f5
Arjan Kuijpers <arjankuijpers@live.nl>
2015-01-10 17:12:27 +0100

    when creating a customAnalytics object, it called initialize as it supposed to do

--- customAnalytics.js
+++ customAnalytics.js
@@ -1,66 +1,68 @@
 /**
- * @author Arjan Kuijpers <arjankuijpers@live.nl>
- */
+* @author Arjan Kuijpers <arjankuijpers@live.nl>
+*/
 
 
 CustomAnalytics = function(trackingId, clientId, osInfo, language)
 {
-    this.trackingID = trackingId;
-    this.AnalyticsURL = "https://google-analytics.com/collect";
-    this.measurementVersion = 1;
-    this.cid = clientId || 0;
-	this.ul = language || this._getLanguage();
-	this.osInfo = osInfo || "null";
+  this.trackingID = trackingId;
+  this.AnalyticsURL = "https://google-analytics.com/collect";
+  this.measurementVersion = 1;
+  this.cid = clientId || 0;
+  this.ul = language || this._getLanguage();
+  this.osInfo = osInfo || "null";
+
+  this.initialize();
 }
 
 CustomAnalytics.prototype = {
-    initialize: function(){
-        //init the Analytics
-        console.log("CA: initialize Custom Analytics");
-    },
-    
-    setDimension: function(fieldName, value){
-        console.log("CA: setDimension not implemented"); 
-    },
-    
-    sendPageView:function(page){
-        var params = "dh=CustomAnalytics.app&dp=" + page + "&dt="+page;
-        this._postToService("pageview", params);
-    },
-    
-    sendEvent: function(category, action, label, value){
-        var params = "ec="+category+"&ea="+action+"&el="+label+"&ev="+value;
-        this._postToService("event", params);
-    },
-    
-    _postToService: function(hitType, aditionalParams, callback) {
-        var params = "v="+this.measurementVersion +
-            "&tid="+ this.trackingID +
-            "&cid="+this.cid +
-            "&t="+hitType +
-			"&ul="+this.ul +
-			"&cm[1][0]*=" + this.osInfo + // Custom metric, you can specify it with what you want.
-            "&"+aditionalParams;
-        
-        
-        var trackingPixel = new Image();
-        trackingPixel.onload = function()
-        {
-            console.log("trackingPixel onload" );
-        }
-        trackingPixel.onerror = function()
-        {
-            console.log("trackingPixel onerror" );
-        }
-        trackingPixel.src = this.AnalyticsURL + "?" + params;
-        //console.log(trackingPixel.src);
-    },
-	
-	_getLanguage: function() {
-		return navigator.language ? navigator.language : null;
-	},
-	
-	setupOS: function(osString) {
-		this.osInfo = osString;
-	},
+  initialize: function(){
+    //init the Analytics
+    console.log("CA: initialize Custom Analytics");
+  },
+
+  setDimension: function(fieldName, value){
+    console.log("CA: setDimension not implemented");
+  },
+
+  sendPageView:function(page){
+    var params = "dh=CustomAnalytics.app&dp=" + page + "&dt="+page;
+    this._postToService("pageview", params);
+  },
+
+  sendEvent: function(category, action, label, value){
+    var params = "ec="+category+"&ea="+action+"&el="+label+"&ev="+value;
+    this._postToService("event", params);
+  },
+
+  _postToService: function(hitType, aditionalParams, callback) {
+    var params = "v="+this.measurementVersion +
+    "&tid="+ this.trackingID +
+    "&cid="+this.cid +
+    "&t="+hitType +
+    "&ul="+this.ul +
+    "&cm[1][0]*=" + this.osInfo + // Custom metric, you can specify it with what you want.
+    "&"+aditionalParams;
+
+
+    var trackingPixel = new Image();
+    trackingPixel.onload = function()
+    {
+      console.log("trackingPixel onload" );
+    }
+    trackingPixel.onerror = function()
+    {
+      console.log("trackingPixel onerror" );
+    }
+    trackingPixel.src = this.AnalyticsURL + "?" + params;
+    //console.log(trackingPixel.src);
+  },
+
+  _getLanguage: function() {
+    return navigator.language ? navigator.language : null;
+  },
+
+  setupOS: function(osString) {
+    this.osInfo = osString;
+  },
 }
