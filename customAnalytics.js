8bf9c19e33cb60782b9046b5c0b7b10eddcb7aa4
Arjan Kuijpers <arjankuijpers@live.nl>
2015-01-11 16:56:11 +0100

    - fixed a merge problem where code was merged wrong, and code was duplicated in the result: removed duplicated code.

--- customAnalytics.js
+++ customAnalytics.js
@@ -67,86 +67,37 @@ CustomAnalytics.prototype = {
     this.osInfo = osString;
   },
 }
-    initialize: function(){
-        //init the Analytics
-        console.log("CA: initialize Custom Analytics");
-    },
 
-    setDimension: function(fieldName, value){
-        console.log("CA: setDimension not implemented");
-    },
-
-    sendPageView:function(page){
-        var params = "dh=CustomAnalytics.app&dp=" + page + "&dt="+page;
-        this._postToService("pageview", params);
-    },
-
-    sendEvent: function(category, action, label, value){
-        var params = "ec="+category+"&ea="+action+"&el="+label+"&ev="+value;
-        this._postToService("event", params);
-    },
-
-    _postToService: function(hitType, aditionalParams, callback) {
-        var params = "v="+this.measurementVersion +
-            "&tid="+ this.trackingID +
-            "&cid="+this.cid +
-            "&t="+hitType +
-            "&ul="+this.ul +
-            "&cm[1][0]*=" + this.osInfo + // Custom metric, you can specify it with what you want.
-            "&"+aditionalParams;
-
-
-        var trackingPixel = new Image();
-        trackingPixel.onload = function()
-        {
-            console.log("trackingPixel onload" );
-        }
-        trackingPixel.onerror = function()
-        {
-            console.log("trackingPixel onerror" );
-        }
-        trackingPixel.src = this.AnalyticsURL + "?" + params;
-        console.log(trackingPixel.src);
-    },
-
-    _getLanguage: function() {
-        return navigator.language ? navigator.language : null;
-    },
-
-    setupOS: function(osString) {
-        this.osInfo = osString;
-    },
-};
 
 
 //overwrite the distlib functions.
 (function() {
-    console.log("try to overwrite distlib");
+  console.log("try to overwrite distlib");
 
-    if((typeof DistLib.GoogleAnalytics === "undefined")) {
-        console.error("Distlib not yet loaded, this needs to be loaded after distlib");
-        return false;
-    }
+  if((typeof DistLib.GoogleAnalytics === "undefined")) {
+    console.error("Distlib not yet loaded, this needs to be loaded after distlib");
+    return false;
+  }
 
-    DistLib.GoogleAnalytics.prototype.initialize = function() {
-        var trackingId = this._trackingId;
-        var clientId = CC.getClientId();
-        var osInfo = navigator.platform;
-        var language = navigator.language;
+  DistLib.GoogleAnalytics.prototype.initialize = function() {
+    var trackingId = this._trackingId;
+    var clientId = CC.getClientId();
+    var osInfo = navigator.platform;
+    var language = navigator.language;
 
-        this.ca = new CustomAnalytics(trackingId, clientId, osInfo, language);
-    }
+    this.ca = new CustomAnalytics(trackingId, clientId, osInfo, language);
+  }
 
-    DistLib.GoogleAnalytics.prototype.setDimension = function(fieldName, value) {
-        this.ca.setDimension(fieldName, value);
-    }
+  DistLib.GoogleAnalytics.prototype.setDimension = function(fieldName, value) {
+    this.ca.setDimension(fieldName, value);
+  }
 
-    DistLib.GoogleAnalytics.prototype.sendPageView = function(page) {
-        this.ca.sendPageView(page);
-    }
+  DistLib.GoogleAnalytics.prototype.sendPageView = function(page) {
+    this.ca.sendPageView(page);
+  }
 
-    DistLib.GoogleAnalytics.prototype.sendEvent = function(category,action,label,value) {
-        this.ca.sendEvent(category,action,label,value);
-    }
+  DistLib.GoogleAnalytics.prototype.sendEvent = function(category,action,label,value) {
+    this.ca.sendEvent(category,action,label,value);
+  }
 
 }());
