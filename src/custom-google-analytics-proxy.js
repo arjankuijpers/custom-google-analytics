e1ae7351bae6f92c986c7429dd179db992ff7645
Arjan Kuijpers <arjankuijpers@live.nl>
2015-10-18 23:25:14 +0200

    - further development of node proxy (javascript) - other proxy added (php) is now available, and stable. WARNING: the php version of the proxy does always return OK, even when an error occured.

--- src/custom-google-analytics-proxy.js
+++ src/custom-google-analytics-proxy.js
@@ -6,31 +6,39 @@
  */
 
 http = require('http');
+https = require('https');
 fs = require('fs');
+
+
+var AnalyticsURL = "https://google-analytics.com/collect";
+
 server = http.createServer( function(req, res) {
 
     console.dir(req.param);
-
-    if (req.method == 'POST') {
-        console.log("POST");
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            console.log("Partial body: " + body);
-        });
-        req.on('end', function () {
-            console.log("Body: " + body);
-        });
+    
+    if (req.method == 'GET') {
+        console.log("get request received.\n" + 
+        req.url);
         res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end("OK");
+        
+        _sendDataToGS(req.url);
+    } else {
+        console.log("WARNING: POST received.");
+        
+        // var body = '';
+        // req.on('data', function (data) {
+        //     body += data;
+        //     console.log("Partial body: " + body);
+        // });
+        
+        // req.on('end', function () {
+        //     console.log("Body: " + body);
+        // });
+        
+        res.writeHead(405, {'Content-Type': 'text/html'});
         res.end('post received');
-    }
-    else
-    {
-        console.log("GET");
-        var html = '<html><body><form method="post" action="http://localhost:3000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
-        //var html = fs.readFileSync('index.html');
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
+        
     }
 
 });
@@ -39,3 +47,26 @@ port = 3000;
 host = '127.0.0.1';
 server.listen(port, host);
 console.log('Listening at http://' + host + ':' + port);
+
+    
+    
+/**
+* This is an system function, do NOT call this function, but use other functions that call this function internally.
+* @private
+*/
+var _sendDataToGS = function(params){
+    console.log("sendDataToGS");
+    var resultURL = AnalyticsURL + params.substring(1,params.length);
+    console.log(resultURL);
+    
+   https.get(resultURL, function(res) {
+        console.log("sendDataToGS - Got response: " + res.statusCode);
+    }).on('error', function(e) {
+        console.log("dropped proxied analytics data.\n sendDataToGS - Got error: " + e.message);
+    });  
+}
+
+
+
+
+
